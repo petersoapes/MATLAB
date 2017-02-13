a57d7587e214e29bb5488aa243b2356248007ccc
APeterson <petersoapes@gmail.com>
2017-02-13 11:26:56 -0600

    Playing with Image Analysis
    further playing with the image analysis functions in MATLAB.

---
+++ ImageAnalysisTest.m
@@ -0,0 +1,189 @@
+tic; % Start timer.
+clc; % Clear command window.
+clearvars; % Get rid of variables from prior run of this m-file.
+fprintf('Running BlobsDemo.m...\n'); % Message sent to command window.
+workspace; % Make sure the workspace panel with all the variables is showing.
+imtool close all;  % Close all imtool figures.
+format long g;
+format compact;
+captionFontSize = 14;
+
+% Check that user has the Image Processing Toolbox installed.
+hasIPT = license('test', 'image_toolbox');
+if ~hasIPT
+	% User does not have the toolbox installed.
+	message = sprintf('Sorry, but you do not seem to have the Image Processing Toolbox.\nDo you want to try to continue anyway?');
+	reply = questdlg(message, 'Toolbox missing', 'Yes', 'No', 'Yes');
+	if strcmpi(reply, 'No')
+		% User said No, so exit.
+		return;
+	end
+end
+
+%load image. change this to allow 3 channel images
+% Read in a standard MATLAB demo image of coins (US nickles and dimes, which are 5 cent and 10 cent coins)
+baseFileName = 'test2red0001.png';%'coins.png'
+folder = fileparts(which(baseFileName)); % Determine where demo folder is (works with all versions).
+fullFileName = fullfile(folder, baseFileName);
+if ~exist(fullFileName, 'file')
+	% It doesn't exist in the current folder.
+	% Look on the search path.
+	if ~exist(baseFileName, 'file')
+		% It doesn't exist on the search path either.
+		% Alert user that we can't find the image.
+		warningMessage = sprintf('Error: the input image file\n%s\nwas not found.\nClick OK to exit the demo.', fullFileName);
+		uiwait(warndlg(warningMessage));
+		fprintf(1, 'Finished running BlobsDemo.m.\n');
+		return;
+	end
+	% Found it on the search path.  Construct the file name.
+	fullFileName = baseFileName; % Note: don't prepend the folder.
+end
+% If we get here, we should have found the image file.
+originalImage = imread(fullFileName);
+% Check to make sure that it is grayscale, just in case the user substituted their own image.
+[rows, columns, numberOfColorChannels] = size(originalImage);
+if numberOfColorChannels > 1
+	promptMessage = sprintf('Your image file has %d color channels.\nThis demo was designed for grayscale images.\nDo you want me to convert it to grayscale for you so you can continue?', numberOfColorChannels);
+	button = questdlg(promptMessage, 'Continue', 'Convert and Continue', 'Cancel', 'Convert and Continue');
+	if strcmp(button, 'Cancel')
+		fprintf(1, 'Finished running BlobsDemo.m.\n');
+		return;
+	end
+	% Do the conversion using standard book formula
+	originalImage = rgb2gray(originalImage);
+end
+
+% Display the grayscale image.
+subplot(3, 3, 1);%ploting 3 by 3, position 1?
+imshow(originalImage);
+% Maximize the figure window.
+set(gcf, 'units','normalized','outerposition',[0 0 1 1]);%gcf is the current figure handle
+% Force it to display RIGHT NOW (otherwise it might not display until it's all done, unless you've stopped at a breakpoint.)
+drawnow;
+caption = sprintf('Image, red snakes');
+title(caption, 'FontSize', captionFontSize);
+axis image; % Make sure image is not artificially stretched because of screen's aspect ratio. %why would image be stretched?
+
+% Just for fun, let's get its histogram and display it.
+[pixelCount, grayLevels] = imhist(originalImage);
+subplot(3, 3, 2); %for position 2 in 3 by 3?
+bar(pixelCount);%bar = bargraph
+title('Histogram of original image', 'FontSize', captionFontSize);
+xlim([0 grayLevels(end)]); % Scale x axis manually.
+grid on;%grid liines in plot
+
+
+% Threshold the image to get a binary image (only 0's and 1's) of class "logical."
+% Method #1: using im2bw()
+%   normalizedThresholdValue = 0.4; % In range 0 to 1.
+%   thresholdValue = normalizedThresholdValue * max(max(originalImage)); % Gray Levels.
+%   binaryImage = im2bw(originalImage, normalizedThresholdValue);       % One way to threshold to binary
+% Method #2: using a logical operation.
+thresholdValue = 120;%100 %120 seems good
+binaryImage = originalImage > thresholdValue; % Bright objects will be chosen if you use >.
+binaryImage = imfill(binaryImage, 'holes');
+
+% Show the threshold as a vertical red bar on the histogram.
+hold on;
+maxYValue = ylim;
+line([thresholdValue, thresholdValue], maxYValue, 'Color', 'r');
+% Place a text label on the bar chart showing the threshold.
+annotationText = sprintf('Thresholded at %d gray levels', thresholdValue);
+% For text(), the x and y need to be of the data class "double" so let's cast both to double.
+text(double(thresholdValue + 5), double(0.5 * maxYValue(2)), annotationText, 'FontSize', 10, 'Color', 'blue');
+text(double(thresholdValue - 70), double(0.94 * maxYValue(2)), 'Background', 'FontSize', 10, 'Color', 'blue');
+text(double(thresholdValue + 50), double(0.94 * maxYValue(2)), 'Foreground', 'FontSize', 10, 'Color', 'blue');
+
+% Display the binary image.
+subplot(3, 3, 3);
+imshow(binaryImage); 
+title('Binary Image, obtained by thresholding', 'FontSize', captionFontSize); 
+
+% Identify individual blobs by seeing which pixels are connected to each other.
+% Each group of connected pixels will be given a label, a number, to identify it and distinguish it from the other blobs.
+% Do connected components labeling with either bwlabel() or bwconncomp().
+labeledImage = bwlabel(binaryImage, 4);     % Label each blob so we can make measurements of it
+% labeledImage is an integer-valued image where all pixels in the blobs have values of 1, or 2, or 3, or ... etc.
+subplot(3, 3, 4);
+imshow(labeledImage, []);  % Show the gray scale image.
+title('Labeled Image, from bwlabel()', 'FontSize', captionFontSize);
+
+% Let's assign each blob a different color to visually show the user the distinct blobs.
+coloredLabels = label2rgb (labeledImage, 'hsv', 'k', 'shuffle'); % pseudo random color labels
+% coloredLabels is an RGB image.  We could have applied a colormap instead (but only with R2014b and later)
+
+subplot(3, 3, 5);%change the number colors from black
+imshow(coloredLabels);
+axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
+caption = sprintf('Pseudo colored labels, from label2rgb().\nBlobs are numbered from top to bottom, then from left to right.');
+title(caption, 'FontSize', captionFontSize);
+
+% Get all the blob properties.  Can only pass in originalImage in version R2008a and later.
+blobMeasurements = regionprops(labeledImage, originalImage, 'all');
+numberOfBlobs = size(blobMeasurements, 1);
+
+% Display the binary image.
+subplot(3, 3, 3);
+imshow(binaryImage); 
+title('Binary Image, obtained by thresholding', 'FontSize', captionFontSize); 
+
+% Identify individual blobs by seeing which pixels are connected to each other.
+% Each group of connected pixels will be given a label, a number, to identify it and distinguish it from the other blobs.
+% Do connected components labeling with either bwlabel() or bwconncomp().
+labeledImage = bwlabel(binaryImage, 8);     % Label each blob so we can make measurements of it
+% labeledImage is an integer-valued image where all pixels in the blobs have values of 1, or 2, or 3, or ... etc.
+subplot(3, 3, 4);
+imshow(labeledImage, []);  % Show the gray scale image.
+title('Labeled Image, from bwlabel()', 'FontSize', captionFontSize);
+
+% Let's assign each blob a different color to visually show the user the distinct blobs.
+coloredLabels = label2rgb (labeledImage, 'hsv', 'k', 'shuffle'); % pseudo random color labels
+% coloredLabels is an RGB image.  We could have applied a colormap instead (but only with R2014b and later)
+subplot(3, 3, 5);%change the number colors from black
+imshow(coloredLabels);
+axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
+caption = sprintf('Pseudo colored labels, from label2rgb().\nBlobs are numbered from top to bottom, then from left to right.');
+title(caption, 'FontSize', captionFontSize);
+
+% Get all the blob properties.  Can only pass in originalImage in version R2008a and later.
+blobMeasurements = regionprops(labeledImage, originalImage, 'all');
+numberOfBlobs = size(blobMeasurements, 1);
+
+% bwboundaries() returns a cell array, where each cell contains the row/column coordinates for an object in the image.
+% Plot the borders of all the coins on the original grayscale image using the coordinates returned by bwboundaries.
+subplot(3, 3, 6);
+imshow(originalImage);
+title('Outlines, from bwboundaries()', 'FontSize', captionFontSize); 
+axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
+hold on;
+boundaries = bwboundaries(binaryImage);
+numberOfBoundaries = size(boundaries, 1);
+for k = 1 : numberOfBoundaries
+	thisBoundary = boundaries{k};
+	plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
+end
+hold off;
+
+textFontSize = 14;	% Used to control size of "blob number" labels put atop the image.
+labelShiftX = -7;	% Used to align the labels in the centers of the coins.
+blobECD = zeros(1, numberOfBlobs);
+% Print header line in the command window.
+fprintf(1,'Blob #      Mean Intensity  Area   Perimeter    Centroid       Diameter\n');
+% Loop over all blobs printing their measurements to the command window.
+for k = 1 : numberOfBlobs           % Loop through all blobs.
+	% Find the mean of each blob.  (R2008a has a better way where you can pass the original image
+	% directly into regionprops.  The way below works for all versions including earlier versions.)
+	thisBlobsPixels = blobMeasurements(k).PixelIdxList;  % Get list of pixels in current blob.
+	meanGL = mean(originalImage(thisBlobsPixels)); % Find mean intensity (in original image!)
+	meanGL2008a = blobMeasurements(k).MeanIntensity; % Mean again, but only for version >= R2008a
+	
+	blobArea = blobMeasurements(k).Area;		% Get area.
+	blobPerimeter = blobMeasurements(k).Perimeter;		% Get perimeter.
+	blobCentroid = blobMeasurements(k).Centroid;		% Get centroid one at a time
+	blobECD(k) = sqrt(4 * blobArea / pi);					% Compute ECD - Equivalent Circular Diameter.
+	fprintf(1,'#%2d %17.1f %11.1f %8.1f %8.1f %8.1f % 8.1f\n', k, meanGL, blobArea, blobPerimeter, blobCentroid, blobECD(k));
+	% Put the "blob number" labels on the "boundaries" grayscale image.
+	text(blobCentroid(1) + labelShiftX, blobCentroid(2), num2str(k), 'Color', 'red', 'FontSize', textFontSize, 'FontWeight', 'Bold');
+end
+
